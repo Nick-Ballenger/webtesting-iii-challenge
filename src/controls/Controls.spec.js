aca205fd79124d387d84be9af760175486d7418c
Lambda_School_Loaner_85 <lambda_school_loaner_85@Lambda-School-Loaner-85s-MacBook-Pro.local>
2019-05-14 23:03:34 -0600

    testing

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,27 @@
 // Test away!
+import React from 'react';
+import { render } from 'react-testing-library';
+import 'react-testing-library/cleanup-after-each';
+import Controls from './Controls';
+import { toBeDisabled } from 'jest-dom';
+
+
+expect.extend({toBeDisabled});
+
+
+describe("Control component tests", ()=>{
+//Lock Test
+test('disabling open and close when locked', () => {
+    const { getByText } = render(<Controls locked />);
+
+    const button = getByText(/close gate/i);
+
+    expect(button).toBeDisabled();
+});
+
+
+
+
+
+
+})
